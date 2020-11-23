3ccdc9d31a62f846007dd660c128e196f4c3db00
Fedor Baart <fedor.baart@deltares.nl>
2020-11-23 14:28:45 +0100

    add ship dataset to frontend

--- digitaltwin-waterway/src/components/FleetComponent.vue
+++ digitaltwin-waterway/src/components/FleetComponent.vue
@@ -6,10 +6,12 @@
       outlined
       v-for="(ship, index) in ships" :key="index">
       <v-card-title>
-        {{ ship.title }}
+        {{ ship['Description (Dutch)'] }}
       </v-card-title>
+      <!-- TODO: add color, icon/image -->
       <v-card-subtitle>
-        {{ ship.subtitle }}
+        Vessel type: {{ ship['Vessel type'] }}, CEMT: {{ ship['CEMT-class'] }}, RWS: {{ ship['RWS-class'] }}
+        Length: {{ ship['Length [m]'] }}m, Beam: {{ ship['Beam [m]']}}m
       </v-card-subtitle>
       <v-progress-linear
         :color="ship.color"
@@ -25,13 +27,22 @@
 export default {
   data () {
     return {
-      ships: [{
-        title: 'Ship A',
-        subtitle: 'Nice vessel',
-        color: 'blue',
-        progress: 40
-      }]
+      ships: []
+    }
+  },
+  mounted () {
+    this.fetchShips().then((ships) => {
+      this.ships = ships
+    })
+  },
+  methods: {
+    async fetchShips () {
+      const resp = await fetch('data/DTV_shiptypes_database.json')
+      const ships = await resp.json()
+      console.log('ships', ships)
+      return ships
     }
   }
+
 }
 </script>
