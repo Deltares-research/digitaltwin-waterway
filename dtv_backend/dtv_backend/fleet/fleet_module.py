f7e32d5be2d03c80beee72fb1203e8472ab9588f
KLEF <frank.klein.schaarsberg@witteveenbos.com>
2020-11-26 08:28:44 +0100

    added functionalities to have proper moving and logging and processes in general

--- dtv_backend/dtv_backend/fleet/fleet_module.py
+++ dtv_backend/dtv_backend/fleet/fleet_module.py
@@ -8,3 +8,24 @@ Created on Mon Nov 23 15:07:26 2020
 # fleet class is used to store all fleet related information
 # move activities should be able to work with the fleet
 # fleet consists of vessels
+
+from dtv_backend.core import vessels as backendVessels
+from dtv_backend.fleet import NPRCfleet
+from dtv_backend.fleet import Dancerfleet
+
+
+#%%
+def loadDTVFleet(env):
+    # load NPRC fleet
+    bulk_fleet = NPRCfleet.provideNPRCfleet(env)
+    # load Dancer fleet
+    container_fleet = Dancerfleet.provideDancerfleet(env)
+    
+    # to be separately returned
+    # container fleet
+    # bulk fleet
+    return
+
+#%%
+def provideFleet(end, vessel_dict):
+    return
