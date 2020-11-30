f40b92e6203e270df33003498d5fcdbe9f50c110
Fedor Baart <fedor.baart@deltares.nl>
2020-11-30 22:21:36 +0100

    cli interface

--- dtv_backend/dtv_backend/cli.py
+++ dtv_backend/dtv_backend/cli.py
@@ -1,38 +1,84 @@
 """Console script for dtv_backend."""
 import sys
 import json
+import logging
 
 import click
 
 # dependencies
-from dtv_backend.core import sites as backendSites
+import dtv_backend.core.sites
+import dtv_backend.core.vessels
 from dtv_backend.processes.single_run_process_fleet import single_run_process_fleet
 
 
 import dtv_backend.dtv_backend as backend
+import dtv_backend.network.network_utilities
 
 @click.group()
 def main(args=None):
     """Console script for dtv_backend."""
+
+    logging.basicConfig(level=logging.INFO)
     click.echo("Starting dtv_backend")
-    # set up an environment
-    env = backend.provide_environment()
-    # pre load the network
-    backend.load_DTV_network_to_env(env)
-    
+
+
     return 0
 
 @main.command()
 @click.argument('input', type=click.File('r'))
 def simulate(input):
     """run a simulation"""
-    
+
     # click.echo("Running a simulation")
-    # config = json.load(input)
-    
-    # config["vessels"]
-    # config["sites"]
-    # config["activites"]
+    config = json.load(input)
+
+    click.echo(f"fleet {config['fleet']}", )
+    click.echo(f"sites {config['sites']}")
+    click.echo(f"climate {config['climate']}")
+    click.echo(f"activities {config['activities']}")
+
+    # set up an environment
+    env = backend.provide_environment()
+
+    # pre load the network
+    backend.load_DTV_network_to_env(env)
+
+    sites = [
+        dtv_backend.core.sites.feature2site(site, env)
+        for site
+        in config['sites']
+    ]
+
+    origin = sites[0]
+    destination = sites[1]
+
+    lobith_discharge = config["climate"]["lobith"]
+
+    max_draught = dtv_backend.network.network_utilities.determine_max_draught_on_path(
+        env.FG,
+        origin,
+        destination,
+        lobith_discharge
+    )
+    click.echo(f"max draught: {max_draught}")
+
+    vessels = [
+        dtv_backend.core.vessels.feature2vessel(feature, env)
+        for feature
+        in config['fleet']
+    ]
+
+    single_run_process_fleet(
+        vessels,
+        env,
+        origin,
+        destination,
+        loader=origin,
+        unloader=destination
+    )
+    env.run()
+    print(origin.container.get_level())
+
     # activity = config["activities"][0]
     # activity["source"]
     # activity["destination"]
@@ -46,10 +92,10 @@ def simulate(input):
     # site["environment"]["discharge"]["lobith"] = 3.0
     # simulation = load_simulation(config)
     # convert vessel config into types
-    
+
     # load network
-    
-    
+
+
     # convert config["sites"] into openCLSim site
     # create activities from sites and loads
 
@@ -70,37 +116,34 @@ def simulate(input):
     # generate visualisations -> qgis, kml, movies
     #
     # cleanup
-    
-    # read config
-    config = json.load(input)
-    
-    # define origin
-    # TODO: specify the correct inputs from the config
-    origin = backendSites.provideSite(env = main.env, 
-                           point = config.origin.point, 
-                           name = config.origin.name,
-                           level = config.origin.level,
-                           loading_rate = config.origin.loading_rate,
-                           unloading_rate = config.origin.unloading_rate)
-    
-    # define destination
-    # TODO: specify the correct inputs from the config
-    destination = backendSites.provideSite(env = main.env, 
-                           point = config.destination.point, 
-                           name = config.destination.name,
-                           level = config.destination.level,
-                           loading_rate = config.destination.loading_rate,
-                           unloading_rate = config.destination.unloading_rate)
-    
-    # add the vessels to the simulation as a fleet
-    # TODO: specify the correct inputs from the config within this function
-    fleet = backend.add_fleet_to_simulation(main.env, origin, config)
-    
-    # perform transport task
-    single_run_process_fleet(fleet, main.env, origin, destination, loader=origin, unloader=destination)
-    
-    
-    pass
+
+    # # define origin
+    # # TODO: specify the correct inputs from the config
+    # origin = backendSites.provideSite(env = main.env,
+    #                        point = config.origin.point,
+    #                        name = config.origin.name,
+    #                        level = config.origin.level,
+    #                        loading_rate = config.origin.loading_rate,
+    #                        unloading_rate = config.origin.unloading_rate)
+
+    # # define destination
+    # # TODO: specify the correct inputs from the config
+    # destination = backendSites.provideSite(env = main.env,
+    #                        point = config.destination.point,
+    #                        name = config.destination.name,
+    #                        level = config.destination.level,
+    #                        loading_rate = config.destination.loading_rate,
+    #                        unloading_rate = config.destination.unloading_rate)
+
+    # # add the vessels to the simulation as a fleet
+    # # TODO: specify the correct inputs from the config within this function
+    # fleet = backend.add_fleet_to_simulation(main.env, origin, config)
+
+    # # perform transport task
+    # single_run_process_fleet(fleet, main.env, origin, destination, loader=origin, unloader=destination)
+
+
+    # pass
 
 
 
