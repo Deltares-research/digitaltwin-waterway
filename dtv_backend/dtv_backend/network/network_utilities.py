a44d29e3a70b5eb6b3087349f37bee0bbdf05fdc
KLEF <frank.klein.schaarsberg@witteveenbos.com>
2020-11-30 13:24:09 +0100

    small mistake in network/network_utilities.py

--- dtv_backend/dtv_backend/network/network_utilities.py
+++ dtv_backend/dtv_backend/network/network_utilities.py
@@ -56,7 +56,7 @@ def determine_min_waterdepth_on_path(graph, origin, destination, discharge_df, l
     #TODO: the file "depth.csv" is missing... this should be loaded as discharge_df
     
     origin_node = find_closest_node(graph, origin.geomerty)
-    destination_node = find_closest_node(graph, origin.geomerty)
+    destination_node = find_closest_node(graph, destination.geomerty)
     
     # determine path and route (subgraph)
     path = nx.dijkstra_path(graph, origin_node, destination_node, weight='Length')
