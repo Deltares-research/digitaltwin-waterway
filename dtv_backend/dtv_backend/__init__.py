539ffca7985fdc53be325a84a642110476d397c5
Fedor Baart <fedor.baart@deltares.nl>
2020-12-04 10:24:38 +0100

    remove nested imports

--- dtv_backend/dtv_backend/__init__.py
+++ dtv_backend/dtv_backend/__init__.py
@@ -3,10 +3,3 @@
 __author__ = """Fedor Baart"""
 __email__ = 'fedor.baart@deltares.nl'
 __version__ = '0.1.0'
-
-from dtv_backend import dtv_backend
-
-from dtv_backend import activities
-from dtv_backend import processes
-from dtv_backend import fleet
-from dtv_backend import core
