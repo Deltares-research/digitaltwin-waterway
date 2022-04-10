a1ccbff3aad472d18bbc1295f046b94fe1fa75d6
Fedor Baart <fedor.baart@deltares.nl>
2022-04-10 16:48:58 +0200

    views update

--- dtv_frontend/src/views/Home.vue
+++ dtv_frontend/src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
 <v-row class="main-row" no-gutters>
-  <v-col class="pa-3 main-row" cols="12" md="5">
+  <v-col class="pa-3 main-row" cols="12" md="7">
     <stepper-component />
   </v-col>
-  <v-col class="pa-0" cols="12" md="7">
+  <v-col class="pa-0" cols="12" md="5">
     <map-component />
   </v-col>
 </v-row>
--- dtv_frontend/src/views/Ships.vue
+++ dtv_frontend/src/views/Ships.vue
@@ -108,7 +108,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   @import '~vuetify/src/styles/main.sass';
 
   $blueprint: map-get($blue, 'darken-4');
